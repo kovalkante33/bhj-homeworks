0f9874160f4d404d4e00828e55294d7367d91d75
BaluevAG <kovalkante@mail.ru>
2025-03-02 17:01:15 +0300

    задание 1.2 решено полностью

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,22 +1,30 @@
-(() => {
+window.game = (() => {
   let playing = true,
     activeHole = 1;
 
   const stop = () => playing = true,
     getHole = index => document.getElementById(`hole${index}`),
     deactivateHole = index =>
-      getHole( index ).className = 'hole',
+      getHole(index).className = 'hole',
     activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
+      getHole(index).className = 'hole hole_has-mole',
     next = () => setTimeout(() => {
-      if ( !playing ) {
+      if (!playing) {
         return;
       }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
+      deactivateHole(activeHole);
+      activeHole = Math.floor(1 + Math.random() * 9);
+      activateHole(activeHole);
       next();
-    }, 800 );
+    }, 800);
 
   next();
+
+  return {
+    playing,
+    getHole,
+    deactivateHole,
+    activateHole,
+    stop,
+  };
 })();
